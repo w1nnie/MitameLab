89b170e367cc6ba734d07c1b6444165f58385d32
winnie <winnie.trap@gmail.com>
2020-11-22 18:11:23 +0900

    fix translateX and delay

--- src/components/IllustMaskDiagonalLine.vue
+++ src/components/IllustMaskDiagonalLine.vue
@@ -24,11 +24,11 @@ export default {
 
 $n: 10
 @mixin tls($i)
-	$x: abs($n / 2 + 0.5 - $i)
+	$x: $n / 2 + 0.5 - abs($n / 2 + 0.5 - $i)
 	@if $i % 2 == 1
-		transform: translateX(calc(50% * #{$x}))
+		transform: translateX(calc(10% * #{$x}))
 	@else
-		transform: translateX(calc(-50% * #{$x}))
+		transform: translateX(calc(-10% * #{$x}))
 
 .illust-mask-diagonal-line-container
 	@include default-mordal
@@ -44,13 +44,14 @@ $n: 10
 			position: absolute
 			width: 60%
 			height: auto
-			top: calc(50vh - 55%)
-			left: calc(50vw - 43%)
+			top: calc(50vh - 350px)
+			left: calc(50vw - 350px)
 
 			@for $i from 1 through $n
 				&.steria:nth-of-type(#{$i})
 					mask-image: linear-gradient(to bottom, transparent 0% calc((#{$i} - 1) * 100% / #{$n}), black calc((#{$i} - 1) * 100% / #{$n}) calc((#{$i} - 1) * 100% / #{$n} + 100% / #{$n}), transparent calc((#{$i} - 1) * 100% / #{$n} + 100% / #{$n}))
-					animation: slideIn-#{$i} 2s ease-out 1s infinite
+					animation: slideIn-#{$i} 2s ease-out infinite
+					animation-delay: #{1 + $i/70}s
 					@keyframes slideIn-#{$i}
 						0%
 							@include tls($i)
@@ -58,7 +59,7 @@ $n: 10
 						3%
 							opacity: 1
 
-						7%
+						6%
 							transform: translateX(0%)
 							opacity: 1
 
